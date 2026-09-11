6d090763b686c55bdac855bdae54fa873afab86c
LingTiannnnn <liuxingyu924@yeah.net>
2026-09-12 00:06:56 +0800

    chore: note Kotlin/Compose compiler version pairing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application") version "8.13.2" apply false
+    // Compose Compiler 1.5.5 官方要求 Kotlin 1.9.20
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.20" apply false
 }
